3a8c275a8863a07a4d4a7259a67b97a4307f9478
hz <3002913319>
2018-05-08 17:55:51 +0800

    添加网络请求，还没有搞定

--- src/com/example/pushtest/MainActivity.java
+++ src/com/example/pushtest/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.pushtest;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Method;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import android.app.Activity;
 import android.app.Notification;
@@ -16,11 +21,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener{
 
 	private Button btnStart;
 	private Button btnStop;
+	private TextView messageText;
+	final static String TAG=MainActivity.class.getName();
+	private String receiveStr="";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,6 +39,7 @@ public class MainActivity extends Activity implements OnClickListener{
 	private void initView() {
 		btnStart=(Button) findViewById(ResUtil.getId(this, "btnStart"));
 		btnStart.setOnClickListener(this);
+		messageText=(TextView)findViewById(ResUtil.getId(this, "messageText"));
 	}
 	
 	private Intent messageIntent=null;
@@ -45,7 +55,7 @@ public class MainActivity extends Activity implements OnClickListener{
 			Log.e("2018581134","2018581134");
 			messageNotification = new Notification();
 			messageNotification.icon=ResUtil.getDrawableId(this, "ic_launcher");
-			messageNotification.tickerText="新消息";
+			messageNotification.tickerText="tickerText";
 			messageNotification.defaults=Notification.DEFAULT_SOUND;
 			messageNotification.flags=Notification.FLAG_AUTO_CANCEL;
 			messageNotificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
@@ -57,12 +67,53 @@ public class MainActivity extends Activity implements OnClickListener{
 			try {
 			Method setLatestEventInfoMethod=notificationClass.getDeclaredMethod("setLatestEventInfo",
 					Context.class,CharSequence.class,CharSequence.class,PendingIntent.class);
-			setLatestEventInfoMethod.invoke(messageNotification, this,"新消息","您有新消息1",messagePendingIntent);
+			setLatestEventInfoMethod.invoke(messageNotification, this,"title","new message",messagePendingIntent);
 			}catch(Exception e) {
 				Log.e("MainActivity","2018581451"+e.toString());
 			}
 			messageNotificationManager.notify(messageNotificationID,
 					messageNotification);
+			String msg=getMessage();
+			messageText.setText(msg);
 		}
+	}
+	
+	private String getMessage()
+	{
+		String str="a";
+		Thread thread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				try {
+					StringBuilder response=new StringBuilder();
+					BufferedReader reader=null;
+					URL url = new URL("http://www.baidu.com");
+					HttpURLConnection connection=(HttpURLConnection)url.openConnection();
+					connection.setRequestMethod("GET");
+					connection.setConnectTimeout(8000);
+					connection.setReadTimeout(8000);
+					InputStream inputStream=connection.getInputStream();
+					reader=new BufferedReader(new InputStreamReader(inputStream));
+					String line;
+					while((line=reader.readLine())!=null) {
+						response.append(line);
+					}
+					receiveStr=response.toString();
+				}catch (Exception e) {
+					Log.e(TAG,e.toString());
+					// TODO: handle exception
+				}
+			}
+		});
+		try {
+			thread.start();
+			thread.join();
+		}catch (Exception e) {
+			// TODO: handle exception
+			Log.e(TAG,e.toString());
+		}
+		return receiveStr;
 	}
 }
