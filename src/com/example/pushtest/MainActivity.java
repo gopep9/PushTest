a1eecc84be2212045061f66056ba1a6a9250ea6e
hz <3002913319>
2018-05-09 09:54:48 +0800

    暂时保存

--- src/com/example/pushtest/MainActivity.java
+++ src/com/example/pushtest/MainActivity.java
@@ -89,7 +89,7 @@ public class MainActivity extends Activity implements OnClickListener{
 				try {
 					StringBuilder response=new StringBuilder();
 					BufferedReader reader=null;
-					URL url = new URL("http://172.25.0.1");
+					URL url = new URL("http://172.25.0.1/pushMessage.php");
 					HttpURLConnection connection=(HttpURLConnection)url.openConnection();
 					connection.setRequestMethod("GET");
 					connection.setConnectTimeout(8000);
